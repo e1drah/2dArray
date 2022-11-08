cc839634c15edebe46a618bd3a88debb146e3dd7
e1drah <113189459+e1drah@users.noreply.github.com>
2022-11-08 10:15:54 -0400

    Completed base

--- 2dArray/Program.cs
+++ 2dArray/Program.cs
@@ -8,55 +8,85 @@ namespace _2dArray
 {
     internal class Program
     {
-        static int[,] timesTable = new int [10, 10]
+        
+        static int[,] timesTable = new int [,]
         {
-            {1,2,3,4,5,6,7,8,9,10},
-            {2,4,6,8,10,12,14,16,18,20},
-            {3,6,9,12,15,18,21,24,27,30},
-            {4,8,12,16,20,24,28,32,36,40},
-            {5,10,15,20,25,30,35,40,45,50},
-            {6,12,18,24,30,36,42,48,54,60},
-            {7,14,21,28,35,42,49,56,63,70},
-            {8,16,24,32,40,48,56,64,72,80},
-            {9,18,27,36,45,54,63,72,81,90},
-            {10,20,30,40,50,60,70,80,90,100},
+            {1,0,1,0,1,0},
+            {0,1,0,1,0,1},
+            {1,0,1,0,1,0},
+            {0,1,0,1,0,1},
+            {1,0,1,0,1,0},
             };
-
+        static int timeTableScaleX = timesTable.GetLength(1);
+        static int timeTableScaleY = timesTable.GetLength(0);
 
         static void Main(string[] args)
         {
-            TableDrawScale(5);
+            TableDraw(6);
             Console.ReadKey();
         }
         static void TableDraw()
         {
-            for (int h = 0; h <= 9; h++)
+            Console.Write("+");
+            for ( int i = 0; i < timesTable.GetLength(1); i++)
             {
-                for (int v = 0; v <= 9; v++)
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.WriteLine();
+
+            for (int vertical = 0; vertical <= (timeTableScaleY - 1); vertical++)
+            {
+                Console.Write("|");
+                for (int horizontal = 0; horizontal <= (timeTableScaleX - 1); horizontal++)
                 {
-                    Console.Write(timesTable[h, v] + ",");
+                    Console.Write(timesTable[vertical, horizontal]);
                 }
+                Console.Write("|");
                 Console.WriteLine();
             }
+            Console.Write("+");
+            for (int i = 0; i < timesTable.GetLength(1); i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.WriteLine();
         }
-        static void TableDrawScale(int scale)
+        static void TableDraw(int scale)
         {
-            for (int h = 0; h <= 9; h++)
+            Console.Write("+");
+            for (int i = 0; i < (timesTable.GetLength(1)* scale); i++)
             {
-                for (int v = 0; v <= 9; v++)
-                {
-                    for (int x = 1; x <= scale; x++)
-                    {
-                        for (int w = 1; w <= scale; w++)
-                        {
-                            Console.Write(timesTable[h, v] + ",");
-                        }
-                        Console.WriteLine();
-                    }
-                    
-                }
-                Console.WriteLine();
+                Console.Write("-");
             }
+            Console.Write("+");
+            Console.WriteLine();
+            for (int vertical = 0; vertical <= (timeTableScaleY - 1); vertical++)
+            { 
+                for (int scaleY = 1; scaleY <= scale; scaleY++ )
+                {
+                    Console.Write("|");
+                    for (int horizontal = 0; horizontal <= (timeTableScaleX - 1); horizontal++)
+                    {
+                        
+                        for (int scalex = 1; scalex <= scale; scalex++)
+                        {
+                            Console.Write(timesTable[vertical, horizontal]);
+                            //Console.ReadKey(true);
+                        }
+                        
+                    }
+                    Console.Write("|");
+                    Console.WriteLine();
+                }
+            }
+            Console.Write("+");
+            for (int i = 0; i < (timesTable.GetLength(1) * scale); i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
         }
 
     }
